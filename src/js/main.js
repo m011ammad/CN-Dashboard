5249c1cb56c27701432ebfeb56c80ef766cdee55
mohammadsjh <mohammadsjh@gmail.com>
2021-09-21 16:30:19 +0430

    Feat(): add some chart to index.php

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,58 @@ desktopMenuIcon.addEventListener('click', e => {
     sideBar.classList.toggle("sidebar-none-active");
     leftSide.classList.toggle("left-side-active");
 })
+
+// ! ---------------
+var ctx1 = document.querySelector(".top-chart");
+new Chart(ctx1, {
+  type: "line",
+  options: {
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        stacked: true,
+        grid: {
+          display: true,
+          color: "rgba(255,99,132,0.2)",
+        },
+      },
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+    },
+  },
+  data: {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+    datasets: [
+      {
+        label: "کاربران جدید",
+        backgroundColor: "#FF8552",
+        borderColor: "#FF8552",
+        borderWidth: 3,
+        data: [165, 189, 320, 181, 156, 355, 277],
+      },
+    ]
+  }
+});
+
+// ! devices-chart
+var ctx2 = document.querySelector(".devices-chart");
+new Chart(ctx2, {
+  type: "doughnut",
+  options: {
+    maintainAspectRatio: false,
+  },
+  data: {
+    labels: ['Android', 'ios', 'windows', 'linux'],
+    datasets: [
+      {
+        backgroundColor: ['green', '#333', 'blue', 'orange'],
+        borderColor: '#fff',
+        borderWidth: 3,
+        data: [51, 20, 19, 10],
+      },
+    ],
+  },
+});
