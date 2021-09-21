b3c417fe4afb20557ac082a2f78023858d78ae0a
mohammadsjh <mohammadsjh@gmail.com>
2021-09-21 13:57:19 +0430

    Feat(): add sidebar toggle on desktop

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,8 @@
-let x = document.querySelector('.header')
-console.log(x);
+let desktopMenuIcon = document.querySelector(".desktop-menu-icon");
+let sideBar = document.querySelector(".right-side");
+let leftSide = document.querySelector(".left-side");
+desktopMenuIcon.addEventListener('click', e => {
+    console.log('c');
+    sideBar.classList.toggle("sidebar-none-active");
+    leftSide.classList.toggle("left-side-active");
+})
