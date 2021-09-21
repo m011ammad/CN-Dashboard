e8925b7ec3573f73b207154b556f3f2c3d3cbb2f
mohammadsjh <mohammadsjh@gmail.com>
2021-09-21 13:30:56 +0430

    Refactor(): covert files to php for better control

---
+++ src/form.php
@@ -0,0 +1,38 @@
+<?php include 'inc/head.html' ?>
+
+<body>
+
+    <div class="right-side">
+        <?php include 'inc/sidebar.html' ?>
+    </div>
+
+    <div class="left-side">
+
+        <header>
+            <div class="container-fluid ">
+                <div class="row header">
+                    <?php include 'inc/left-header.html' ?>
+                </div>
+            </div>
+        </header>
+
+        <main>
+            <div class="container-fluid">
+                <div class="left-main">
+                    <?php include 'inc/left-main__form.html' ?>
+                </div>
+            </div>
+        </main>
+
+        <footer>
+            <?php include 'inc/footer.html' ?>
+        </footer>
+
+    </div>
+
+</body>
+
+<script src="../node_modules/jquery/dist/jquery.min.js"></script>
+<script src="js/main.js"></script>
+
+</html>
